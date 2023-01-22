af73a31b67bce89ef0537da5e907da652958b9f3
Arick <iarick.vigasi@gmail.com>
2023-01-22 17:25:38 +0200

    Update tbot to use latest telegram lib

--- tbot.py
+++ tbot.py
@@ -5,7 +5,8 @@ Source: https://github.com/python-telegram-bot/python-telegram-bot/blob/master/e
 """
 import os
 import logging
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 
 PORT = int(os.environ.get('PORT', 5000))
 TELEGRAM_API_TOKEN = os.environ['TELEGRAM_API_TOKEN']
@@ -22,9 +23,8 @@ logger = logging.getLogger(__name__)
 
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
-def start(update, context):
-    """Send a message when the command /start is issued."""
-    update.message.reply_text('Hi!')
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
 def help(update, context):
     """Send a message when the command /help is issued."""
@@ -40,34 +40,20 @@ def error(update, context):
 
 def main():
     """Start the bot."""
-    # Create the Updater and pass it your bot's token.
-    # Make sure to set use_context=True to use the new context based callbacks
-    # Post version 12 this will no longer be necessary
-    updater = Updater(TELEGRAM_API_TOKEN, use_context=True)
-
-    # Get the dispatcher to register handlers
-    dp = updater.dispatcher
+    application = ApplicationBuilder().token(TELEGRAM_API_TOKEN).build()
 
     # on different commands - answer in Telegram
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
-
-    # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("help", help))
 
     # log all errors
-    dp.add_error_handler(error)
+    application.add_error_handler(error)
 
     # Start the Bot
-    updater.start_webhook(listen="0.0.0.0",
+    application.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
                           url_path=TELEGRAM_API_TOKEN)
-    updater.bot.setWebhook('https://chat-gpt-telegeram-bridge.herokuapp.com/' + TELEGRAM_API_TOKEN)
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
-    updater.idle()
+    application.bot.setWebhook('https://chat-gpt-telegeram-bridge.herokuapp.com/' + TELEGRAM_API_TOKEN)
 
 if __name__ == '__main__':
     main()
